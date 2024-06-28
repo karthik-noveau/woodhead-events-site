8b4cc89745e57a0cf61d5f910c08a1a46800d79b
karthik <karthik5112001@gmail.com>
2024-06-28 15:35:29 +0530

    mobile header added

--- src/components/header/mobile/index.jsx
+++ src/components/header/mobile/index.jsx
@@ -1,7 +1,75 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Sling as Hamburger } from "hamburger-react";
+
 import styles from "./style.module.css";
+import "./hamburger.override.css";
+
+import Logo from "../image/logo.svg";
 
 export const MobileHeader = () => {
-  return <div className={styles.headeWrapper}>
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
 
-  </div>;
+  const onMenuClick = (path) => {
+    setIsOpen(false);
+    navigate(path);
+  };
+
+  return (
+    <React.Fragment>
+      <div className={styles.headerWrapper}>
+        <div className={styles.leftInfo}>
+          <Link to="/" onClick={() => setIsOpen(false)}>
+            <img src={Logo} alt="logo" />
+          </Link>
+        </div>
+        <div
+          className={`${styles.rightInfo} hamburgerMenu ${
+            isOpen ? "closeIcon" : "openIcon"
+          }`}
+        >
+          <Hamburger
+            direction="right"
+            duration={0.7}
+            size={29.2}
+            toggled={isOpen}
+            toggle={() => setIsOpen(!isOpen)}
+          />
+        </div>
+      </div>
+
+      <div
+        className={`${styles.popup} ${
+          isOpen ? styles.popupShow : styles.popupHide
+        }`}
+      >
+        <div className={styles.menuContainer}>
+          <div className={styles.menuItemsContainer}>
+            <p className={styles.menuItem} onClick={() => onMenuClick("/")}>
+              Home
+            </p>
+            <p
+              className={styles.menuItem}
+              onClick={() => onMenuClick("/story")}
+            >
+              Story
+            </p>
+            <p
+              className={styles.menuItem}
+              onClick={() => onMenuClick("/portfolio")}
+            >
+              Portfolio
+            </p>
+          </div>
+          <div
+            className={styles.lastItem}
+            onClick={() => onMenuClick("/connect")}
+          >
+            Connect
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
